38487dabc80acf04d38d999e9f6d2b5c40f3e741
Lucas Alves <lucas.doamaralalves@gmail.com>
2017-04-06 01:40:56 -0300

    Projeto inicial
    Somente os arquivos necessário para iniciar o projeto SpringBoot.
    
    Signed-off-by: Lucas Alves <lucas.doamaralalves@gmail.com>

---
+++ src/main/java/com/crud/StuCrudApplication.java
@@ -0,0 +1,12 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuCrudApplication.class, args);
+	}
+}
